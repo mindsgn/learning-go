b99a128f4a115c99e25a531044ba8efdb7c1818b
mindsgn <seni.tembe@protonmail.com>
2023-09-21 16:04:20 +0200

    update

--- lesson_2/main.go
+++ lesson_2/main.go
@@ -7,7 +7,7 @@ func main(){
 	const numberOfTickets uint = 10
 	var remainingTickets uint = numberOfTickets
 	var username string
-	var userTickets int
+	var userTickets uint
 
 	fmt.Printf("Welcome to %v\n", applicationName)
 	fmt.Printf("We have a total of %v tickets and %v are still available \n", numberOfTickets, remainingTickets)
@@ -21,6 +21,8 @@ func main(){
 	fmt.Printf("Enter number of Tickets: ")
 	fmt.Scan(&userTickets)
 
-	fmt.Printf("Thank you %v for booking %v, you will receive a confirmation sooner to the event date.", username, userTickets)
-	//fmt.Printf("User %v book %v tickets. \n", username, userTickets)
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("Thank you %v for booking %v, you will receive a confirmation sooner to the event date.\n", username, userTickets)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, applicationName)
 }
