469ed07d23df299a1e6ddff964b2006222d39c21
mindsgn <seni.tembe@protonmail.com>
2023-10-03 21:41:38 +0200

    array

--- lesson_2/main.go
+++ lesson_2/main.go
@@ -8,6 +8,7 @@ func main(){
 	var remainingTickets uint = numberOfTickets
 	var username string
 	var userTickets uint
+	var bookings [10]string
 
 	fmt.Printf("Welcome to %v\n", applicationName)
 	fmt.Printf("We have a total of %v tickets and %v are still available \n", numberOfTickets, remainingTickets)
@@ -20,9 +21,16 @@ func main(){
 
 	fmt.Printf("Enter number of Tickets: ")
 	fmt.Scan(&userTickets)
+	
+	bookings[0] = "@"+username
+
+	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("Array Type: %T\n", bookings)
+	fmt.Printf("Array length: %v\n", len(bookings))
 
 	remainingTickets = remainingTickets - userTickets
-
+	
 	fmt.Printf("Thank you %v for booking %v, you will receive a confirmation sooner to the event date.\n", username, userTickets)
 	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, applicationName)
 }
